84b666207e02affc36633377649ddbc77de2c048
mxcl <mxcl@me.com>
2021-12-04 08:00:09 -0500

    Configure count of recs (with default)

--- index.ts
+++ index.ts
@@ -3,18 +3,21 @@ import { QuineAPI } from "./QuineAPI";
 import { getInput } from '@actions/core';
 const cronParser = require('cron-parser');
 
+type PublishIn = 'porter-issue' | 'separate-issue';
 
-async function action(publishIn?: string){
+async function action(publishIn?: PublishIn) {
   const auth = new Authentication();
   const quineAccessToken = await auth.getQuineAccessToken();
-
   const auth0UserInfo = await auth.auth0Auth.getUserInfo(quineAccessToken);
+
+  const desiredRecsCount = getNumberInput('count') ?? publishIn == 'porter-issue' ? 3 : undefined
+  // ^^ if unspecified we default to the API default unless we’re adding to the porter issue
 
   console.log('Received Auth0 user info.');
   const quineAPI = new QuineAPI(quineAccessToken, auth0UserInfo);
   const quineUserId = await quineAPI.getQuineUserId();
   console.log('Received Quine userId');
-  const repoRecommendations = await quineAPI.getRepoRecommendations(Number(quineUserId), [{group: "all"}]);
+  const repoRecommendations = await quineAPI.getRepoRecommendations(Number(quineUserId), [{group: "all"}], desiredRecsCount);
   console.log('Received repo recommendations.');
   const repoDetails = await quineAPI.getReposInfo(Number(quineUserId), repoRecommendations.map(repo => repo.repo_id));
   console.log('Received repos info.');
@@ -48,9 +51,14 @@ function compareDates(date1: Date, date2: Date) {
   return d1Params.day === d2Params.day && d1Params.month === d2Params.month && d1Params.year === d2Params.year;
 }
 
+function getNumberInput(key: string): number | undefined {
+  const n = parseInt(getInput(key));
+  return Number.isNaN(n) ? undefined : n;
+}
+
 async function main () {
   const runCron = getInput('run-cron');
-  const publishIn = getInput('publish-in');
+  const publishIn = getInput('publish-in') as PublishIn;
   if (runCron) {
     const parseRes = cronParser.parseExpression(runCron).prev();
     const currentTime = new Date();
